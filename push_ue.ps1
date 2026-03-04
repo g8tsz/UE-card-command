165fb4d608b17f7342e04e22b609b677e41f392b
lorddummy <nickh4233@gmail.com>
2026-03-03 20:53:58 -0700

    Remove push script

--- push_ue.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location $PSScriptRoot
-$msg = "Initial commit: UE Card Command - form generator for order/assist/reorder Discord commands"
-git add .
-git commit -m $msg
-git remote add origin https://github.com/g8tsz/UE-card-command.git 2>$null
-git branch -M main
-git push -u origin main
